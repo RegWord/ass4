2dc0cda5c4e54d8d0f2646219461ebb3a992d1b2
Rauan Aueskhanov <fernandezz228@mail.ru>
2024-05-26 17:39:01 +0500

    Initial commit

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch<Node> extends Search<Node> {
+    public BreadthFirstSearch(MyGraph<Node> graph, Node source) {
+        super(source);
+
+        bfs(graph, source);
+    }
+
+    private void bfs(MyGraph<Node> graph, Node current) {
+        marked.add(current);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            Node v = queue.remove();
+
+            for (Node vertex : graph.adjacencyList(v)) {
+                if (!marked.contains(vertex)) {
+                    marked.add(vertex);
+                    edgeTo.put(vertex, v);
+                    queue.add(vertex);
+                }
+            }
+        }
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class Node<T> {
+    private T data;
+    private Map<Node<T>, Double> adjacentNodes;
+}
